87e45057ab66a5cfe80e0289ff4d523fb41e3a80
KLFD9 <tonikkpro@gmail.com>
2025-02-13 11:39:04 +0100

    Add Workshop component and update App layout: replace FeaturesGrid with Workshop and remove unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
-import { FeaturesGrid } from './components/FeaturesGrid';
+{/*import { FeaturesGrid } from './components/Workshop';*/}
 import { CommunityGallery } from './components/CommunityGallery';
 import { ArticlePage } from './components/ArticlePage';
 import { Footer } from './components/Footer';
 import { OpenSourceBanner } from './components/OpenSourceBanner';
-import YoutubeLogsPage from './pages/YoutubeLogsPage';
 import SpaceflixPage from './pages/SpaceflixPage';
 import ToolsPage from './pages/ToolsPage';
 import SpaceCalcPage from './pages/SpaceCalcPage';
@@ -18,7 +17,7 @@ const HomePage: React.FC = () => {
     <main>
       <Hero />
       <OpenSourceBanner />
-      <FeaturesGrid />
+      {/*<Workshop />*/}
       <section className="social-section" id="social-section"></section>
     </main>
   );
@@ -33,7 +32,6 @@ const App: React.FC = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/gallery" element={<CommunityGallery />} />
           <Route path="/article/:id" element={<ArticlePage />} />
-          <Route path="/youtube-logs" element={<YoutubeLogsPage />} />
           <Route path="/spaceflix" element={<SpaceflixPage />} />
           <Route path="/tools" element={<ToolsPage />} />
           <Route path="/tools/spacecalc" element={<SpaceCalcPage />} />
